8c0a98990ac33ccca741dbfb0881e535d8baf10b
Ayase Minori <ayane@vignetteapp.org>
2024-02-18 02:55:56 +0800

    Finalize parser class for PMX
    Signed-off-by: Ayase Minori <ayane@vignetteapp.org>

--- lib/dart_mmd.dart
+++ lib/dart_mmd.dart
@@ -7,11 +7,10 @@ import 'package:dart_mmd/pmx.dart';
 class PMXParser {
   static PMX parse(
       ByteBuffer buffer, Function(Error err, dynamic data) callback) {
-    var model = new PMX(buffer, callback);
+    var model = PMX(buffer, callback);
 
-    if (callback != null) {
-      callback(Error(), model.value);
-    }
+    callback(Error(), model.value);
+
     return model;
   }
 }
