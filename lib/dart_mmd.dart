4e73202d55392417a79179992039a8aa96fa069c
Ayase Minori <ayane@vignetteapp.org>
2024-02-18 03:16:54 +0800

    Add dart docs option
    Signed-off-by: Ayase Minori <ayane@vignetteapp.org>

--- lib/dart_mmd.dart
+++ lib/dart_mmd.dart
@@ -4,6 +4,11 @@ import 'dart:typed_data';
 
 import 'package:dart_mmd/pmx.dart';
 
+/// Parses a Polygon Model eXtended file from a buffer.
+/// 
+/// The [buffer] parameter is the buffer to parse.
+/// The [callback] parameter is the callback function to call when the parsing is complete.
+/// It takes an [Error] object as the first parameter (or `null` if no error occurred) and the parsed model data as the second parameter.
 class PMXParser {
   static PMX parse(
       ByteBuffer buffer, Function(Error err, dynamic data) callback) {
