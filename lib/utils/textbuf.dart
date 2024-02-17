6deefef4122cab363a24498feea4bd5c41e3d2af
Ayase Minori <ayane@vignetteapp.org>
2024-02-18 01:31:13 +0800

    Finish PMXMorph implementation
    Signed-off-by: Ayase Minori <ayane@vignetteapp.org>

--- lib/utils/textbuf.dart
+++ lib/utils/textbuf.dart
@@ -1,5 +1,6 @@
 library dart_mmd;
 
+@Deprecated("Dart already supports this functionality with the 'String' class. This class is no longer used.")
 /// Represents a text buffer with a specified length and value.
 class TextBuf {
   late String value;
